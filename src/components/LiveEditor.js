694be53f24f00e449a7813a6d6643d53247d8104
topscores <arnupharp@larngeartech.com>
2016-08-11 18:22:10 +0700

    Fix style issues

--- src/components/LiveEditor.js
+++ src/components/LiveEditor.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class LiveEditor extends Component { // eslint-disable-line react/prefer-stateless-function
+class LiveEditor extends Component {
   render() {
     return (
       <div className="live-editor">
